17df04716642d8a40714e0cc61de373bd4e89ff4
AffandiSupriadi <122225654+AffandiSupriadi@users.noreply.github.com>
2023-01-10 22:45:01 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,7 +24,7 @@ st.write(
 st.write("---")
 st.subheader("Decibel")
 st.write("Decibel (dB) adalah satuan yang sering digunakan sebagai skala penguatan dalam rangkaian elektronika seperti rangkaian pada peralatan komunikasi dan audio. Besaran yang menggunakan skala penguatan decibel tersebut diantaranya seperti penguatan pada daya, arus, dan tegangan. Pada dasarnya decibel adalah satuan yang menggambarkan suatu perbandingan/rasio. Decibel bisa disingkat menjadi “dB” ini bisa diartikan sebagai perbandingan antara dua besaran dalam skala logaritma.")
-st.write("Umumnya desibel merupakan turunan dari besaran Bel, dimana 1 desibel sama dengan 1/10 Bel atau 0,1 Bel. Dalam prakteknya, para engineer atau fisikawan cenderung lebih nyaman menggunakan satuan Decibel (dB) daripada satuan Bel. Hal ini dikarenakan untuk menghindari kebanyakan angka dibelakang koma dalam menghitungnya. Dalam perhitungan decibel, gain/penguatan suatu sinyal akan ditandai dengan tanda “+” (positif), sedangkan pelemahan/loss akan ditandai dengan tanda “-” (negatif). Maka dengan demikian, jika sinyal output +6dB dari sinyal input maka hal ini menandakan terjadinya penguatan output sebanyak 6dB dari sinyal input. Sebaliknya jika sinyal output -2dB dari sinyal input yang artinya yaitu sudah terjadi pelemahan sinyal output sebanyak 2dB terhadap sinyal input.")
+st.write("Umumnya decibel merupakan turunan dari besaran Bel, dimana 1 decibel sama dengan 1/10 Bel atau 0,1 Bel. Dalam prakteknya, para engineer atau fisikawan cenderung lebih nyaman menggunakan satuan Decibel (dB) daripada satuan Bel. Hal ini dikarenakan untuk menghindari kebanyakan angka dibelakang koma dalam menghitungnya. Dalam perhitungan decibel, gain/penguatan suatu sinyal akan ditandai dengan tanda “+” (positif), sedangkan pelemahan/loss akan ditandai dengan tanda “-” (negatif).")
 st.write("[akun instagram saya](https://www.instagram.com/affn_s/)")
 
 st.write("---")
@@ -34,12 +34,12 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
         st.subheader("Rumus rasio daya")
-        st.image("dayalog.jpg")
+        st.image("dayalog.jpg",width=400)
 with col2:
         st.subheader("Rumus rasio arus")
-        st.image("arlog.jpg")
+        st.image("arlog.jpg",width=400)
 with col3:
         st.subheader("Rumus rasio tegangan")
-        st.image("teglog.jpg")
+        st.image("teglog.jpg",width=400)
     
 st.write("---")
